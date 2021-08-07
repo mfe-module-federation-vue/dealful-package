e1b2028bf9748346b8e13fda442f91c50e2a5ec2
Tássio <tassio72@hotmail.com>
2021-08-07 18:12:50 -0300

    change export

--- src/helpers/nanoevents.ts
+++ src/helpers/nanoevents.ts
@@ -1,7 +1,7 @@
 import { createNanoEvents } from "nanoevents";
 
 import { EVENT_KEYS } from "../keys/events";
-import { User } from "@/model/User";
+import { User } from "../model/User";
 
 interface UserEvents {
   [EVENT_KEYS.CHANGE_USER]: (emitCallback: () => User) => void;
